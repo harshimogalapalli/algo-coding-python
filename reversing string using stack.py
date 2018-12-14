d1aec2038a49a17cc750d1da14557e3a4eca6903
Harshit Mogalapalli <38498339+harshimogalapalli@users.noreply.github.com>
2018-12-14 21:47:53 +0530

    Add files via upload

---
+++ reversing string using stack.py
@@ -0,0 +1,15 @@
+import sys
+import stack
+def reverse_string_with_stack(str1):
+   s = stack.Stack()
+   revStr = ''
+   for c in str1:
+     s.push(c)
+   while not s.isEmpty():
+     revStr += s.pop()
+   print(revStr)  
+   
+  
+if __name__ == '__main__':
+    str1 = "python is the best language"
+    reverse_string_with_stack(str1)
